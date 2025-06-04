9970eba7cf4064210e43d010893e535f3a501832
apurvagargote <apurvagargote@gmail.com>
2025-06-04 13:20:21 +0530

    Prepare for GitHub Pages deploy

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,6 +7,14 @@ const PDFFrame = styled.iframe`
   width: 100%;
   height: 800px;
   border: 1px solid var(--border-color);
+  
+  @media (max-width: 768px) {
+    height: 500px;
+  }
+  
+  @media (max-width: 576px) {
+    height: 400px;
+  }
 `;
 
 const Resume = () => {
