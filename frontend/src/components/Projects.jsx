7df1046a229f147e8ca9b55b6b5140d7b505d21d
apurvagargote <126280012+apurvagargote@users.noreply.github.com>
2025-08-01 22:18:12 +0530

    Clean up unused imports in Projects component

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col, Card, Button, Badge } from 'react-bootstrap';
-import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
+import { FaGithub} from 'react-icons/fa';
 
 
 const Projects = () => {
